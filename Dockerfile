298c8da94e42f70f4d645cd9cc764bd7c5987cf2
Joshua Bloom <joshbloom@google.com>
2020-04-21 15:24:44 -0700

    Created html template and updated Dockerfile to use gunicorn

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
-FROM python:3-alpine
+# Use the official lightweight Python image.
+# https://hub.docker.com/_/python
+FROM python:3.7-slim
 
-COPY . /app
-# Create and change to the app directory.
-WORKDIR /app
+# Copy local code to the container image.
+ENV APP_HOME /app
+WORKDIR $APP_HOME
+COPY . ./
 
+# Install production dependencies.
 RUN pip install -r requirements.txt
-# Service must listen to $PORT environment variable.
-# This default value facilitates local development.
-ENV PORT 8080
+RUN pip install gunicorn
 
-# Run the web service on container startup.
-CMD [ "python", "app.py" ]
+# Run the web service on container startup. Here we use the gunicorn
+# webserver, with one worker process and 8 threads.
+# For environments with multiple CPU cores, increase the number of workers
+# to be equal to the cores available.
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 app:app
--- app.py
+++ app.py
@@ -2,35 +2,20 @@ import os
 import logging
 import redis
 
-from flask import Flask
+from flask import Flask, render_template
 
 # Change the format of messages logged to Stackdriver
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 app = Flask(__name__)
 redisHost = os.getenv('redis_host')
+print(redisHost)
 redisPassword = os.getenv('redis_password')
 
 
 @app.route('/')
-def home():
-    val = getRedisValue('foo')
-    html = """
-<html>
- <head>
-  <title>
-   Google Cloud Run - Sample Python Flask Example
-  </title>
- </head>
- <body>
-  <p>Hello Google Cloud Run World!</p> <p>THE VALUE OF FOO IS """
-    html += str(val)
-    html += """ </p>
-  <a href="https://cloud.google.com/run/" target="_blank">Google Cloud Run Website</a>
- </body>
-</html>
-"""
-    return html
+def index():
+    return render_template('index.html', val=getRedisValue('foo'))
 
 
 def getRedisValue(keyname):
